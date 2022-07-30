9c7b10f516d00efd4bcd3342c3ed06fc48de3c2d
AMNOTSIRVOID <brotales26@gmail.com>
2022-07-29 20:45:12 -0400

    nocountdown

---
+++ scripts/nocountdown.hx
@@ -0,0 +1,12 @@
+function onCountdown(val:Int) {
+    switch(val) {
+        case 3:
+          return false;
+        case 2:
+            return false;
+        case 1:
+            return false;
+        case 0:
+            return false;
+    }
+}
