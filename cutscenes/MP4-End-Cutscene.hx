7149a82d1fa072fc1f51523e3a8c03d34ca0ca7a
AMNOTSIRVOID <brotales26@gmail.com>
2022-08-27 17:21:20 -0400

    cum

--- cutscenes/MP4-End-Cutscene.hx
+++ cutscenes/MP4-End-Cutscene.hx
@@ -1,33 +1,32 @@
-// TO USE, ADD A "c.mp4" FILE IN YOUR VIDEOS FOLDER. IT WILL BE HANDLED AUTOMATICALLY
-var wasWidescreen = PlayState.isWidescreen;
-var videoSprite:FlxSprite = null;
-var mFolder = Paths_.modsPath;
-var path = Paths.video(PlayState.song.song + "-cutscene", 'mods/' + PlayState_.songMod);
+// TO USE, ADD A "yoursong-end-cutscene.mp4" FILE IN YOUR VIDEOS FOLDER. IT WILL BE HANDLED AUTOMATICALLY
 
-function onCountdown() {
+function create() {
+    var mFolder = Paths_.modsPath;
     
+    var path = Paths.video(PlayState.song.song + "-end-cutscene", 'mods/' + PlayState_.songMod);
     trace(path);
     if (!Assets.exists(path)) {
         trace("Video not found.");
-
+        end();
+        return;
     }
 
-    
+    var wasWidescreen = PlayState.isWidescreen;
+    var videoSprite:FlxSprite = null;
     
     PlayState.isWidescreen = false;
     PlayState.camHUD.bgColor = 0xFF000000;
-	pauseMusic.volume = 0;
     videoSprite = MP4Video.playMP4(Assets.getPath(path),
         function() {
             PlayState.remove(videoSprite);
             PlayState.isWidescreen = wasWidescreen;
             PlayState.camHUD.bgColor = 0x00000000;
-            startCountdown();
+            end();
         },
         // If midsong.
         false, FlxG.width, FlxG.height);
 
     videoSprite.cameras = [PlayState.camHUD];
     videoSprite.scrollFactor.set();
-    add(videoSprite);
+    PlayState.add(videoSprite);
 }
